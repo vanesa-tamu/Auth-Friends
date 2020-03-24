895d08da93cc7822107ab8ca03c1373770e0dc09
vanesa-tamu <vahe5495@tamu.edu>
2020-03-24 15:49:32 -0500

    Login component almost complete and app structure

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 import './App.css';
 import Login from './components/auth/Login.js'
-
+import Friends from './components/Friends.js' 
+// /friends
 function App() {
   return (
     <div className="App">
--- friends/src/components/auth/Login.js
+++ friends/src/components/auth/Login.js
@@ -1,18 +1,46 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { axiosWithAuth } from '../../utils/axiosWithAuth.js'
 
 const Login = () => {
     const history = useHistory();
+    const [ credentials, setCredentials ] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = e => {
+        const { name, value } = e.target
+
+        setCredentials({
+            ...credentials,
+            [name]: value
+        })
+    }
+
+    const handleLogin = e => {
+        e.preventDefault()
+        axiosWithAuth()
+            .post('/api/login', credentials)
+            .then(res => {
+                console.log('LOGIN RES: ')
+                localStorage.setItem('token', JSON.stringify(res.data.payload))
+                // history.push('/friends')
+            })
+            .catch(err => console.error('ERROR IN LOGIN', err))
+    }
 
     return(
         <div>
             <h1>Please Login!</h1>
-            <form>
+            <form onSubmit={handleLogin}>
                 <label htmlFor='username'>Username</label>
                 <input
                     id='username' 
                     type='text'
                     name='username'
+                    value={credentials.username}
+                    onChange={handleChange}
                 />
 
                 <label htmlFor='password'>Password</label>
@@ -20,6 +48,8 @@ const Login = () => {
                     id='password'
                     name='password'
                     type='password'
+                    value={credentials.password}
+                    onChange={handleChange}
                 />
                 <button type='submit'>Login</button>
             </form>
